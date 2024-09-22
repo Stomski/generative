8dec06b4f270a3c3825d2744c7d2aba71d6e4e76
Robert Stomski <stomski.robert@gmail.com>
2024-09-22 07:51:20 -0600

    spiro canvas appearing

---
+++ src/app/fourier-spiro/fourier-spiro.component.ts
@@ -0,0 +1,36 @@
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fourier-spiro',
+  standalone: true,
+  imports: [],
+  templateUrl: './fourier-spiro.component.html',
+  styleUrl: './fourier-spiro.component.css',
+})
+export class FourierSpiroComponent {
+  @ViewChild('fSpiroCanvas', { static: false })
+  canvasRef!: ElementRef<HTMLCanvasElement>;
+  private ctx!: CanvasRenderingContext2D | null;
+
+  setCanvasSize() {
+    const canvas = this.canvasRef.nativeElement;
+    canvas.width = window.innerWidth * 0.9; // 80% of the window's width
+    canvas.height = window.innerHeight * 0.7; // 60% of the window's height
+  }
+
+  ngAfterViewInit() {
+    const canvas = this.canvasRef.nativeElement;
+    this.ctx = canvas.getContext('2d');
+    this.setCanvasSize();
+    this.drawOnCanvas();
+  }
+
+  drawOnCanvas() {
+    if (this.ctx) {
+      const context = this.ctx;
+      const canvas = this.canvasRef.nativeElement;
+
+      context.fillRect(0, 0, canvas.width, canvas.height);
+    }
+  }
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TreeComponent } from './tree/tree.component';
 import { RoseComponent } from './rose/rose.component';
+import { FourierSpiroComponent } from './fourier-spiro/fourier-spiro.component';
 
 const routeConfig: Routes = [
   {
@@ -15,6 +16,11 @@ const routeConfig: Routes = [
     title: 'Fractal Tree',
   },
   { path: 'rose', component: RoseComponent, title: 'Rhodonea Roses' },
+  {
+    path: 'fourierSpiro',
+    component: FourierSpiroComponent,
+    title: 'Fourier Spirograph',
+  },
 ];
 
 export default routeConfig;
