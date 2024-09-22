421f244a520ea07a2a3f4eaf9d3565e76748fdbb
Robert Stomski <stomski.robert@gmail.com>
2024-09-22 09:28:38 -0600

    animation working

--- src/app/fourier-spiro/fourier-spiro.component.ts
+++ src/app/fourier-spiro/fourier-spiro.component.ts
@@ -22,15 +22,61 @@ export class FourierSpiroComponent {
     const canvas = this.canvasRef.nativeElement;
     this.ctx = canvas.getContext('2d');
     this.setCanvasSize();
-    this.drawOnCanvas();
+    window.requestAnimationFrame(() =>
+      this.drawOnCanvas(this.radius, this.angle, this.numPoints)
+    );
   }
 
-  drawOnCanvas() {
+  radius: number = 100;
+  angle: number = 0;
+  numPoints: number = 50;
+
+  drawOnCanvas(radius: number, angle: number, numPoints: number) {
     if (this.ctx) {
       const context = this.ctx;
       const canvas = this.canvasRef.nativeElement;
 
+      context.fillStyle = 'rgb(0,0,0)';
       context.fillRect(0, 0, canvas.width, canvas.height);
+      context.save();
+
+      context.translate(canvas.width / 2, canvas.height / 2);
+      context.lineWidth = 1;
+      context.strokeStyle = `rgb(255, 0, 0)`; // Red
+
+      context.fillStyle = 'rgb(255,255,255)';
+      context.fillRect(0, 0, 2, 2);
+      let numPoints = 50;
+
+      function drawcircle(numPoints: number, radius: number) {
+        for (let i = 0; i < numPoints; i++) {
+          let x = radius * Math.cos((Math.PI * 2 * i) / numPoints);
+          let y = radius * Math.sin((Math.PI * 2 * i) / numPoints);
+          let lastX = radius * Math.cos((Math.PI * 2 * i - 1) / numPoints);
+          let lastY = radius * Math.sin((Math.PI * 2 * i - 1) / numPoints);
+          if (i === 0) {
+            console.log('loop start');
+            context.beginPath();
+            context.moveTo(x, y);
+          } else if (i === numPoints - 1) {
+            console.log('loopend', window.requestAnimationFrame);
+            context.lineTo(lastX, lastY);
+            context.closePath();
+            context.stroke();
+          } else {
+            context.fillRect(x, y, 1, 1);
+            context.lineTo(lastX, lastY);
+          }
+        }
+      }
+      drawcircle(numPoints, radius);
+
+      context.restore();
     }
+
+    this.radius++;
+    window.requestAnimationFrame(() =>
+      this.drawOnCanvas(this.radius, this.angle, this.numPoints)
+    );
   }
 }
