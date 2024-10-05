dbbc69cd0b832f45367b501de6f80913a0504c9b
Robert Stomski <stomski.robert@gmail.com>
2024-10-05 16:40:48 -0600

    boids is about to pop off

--- src/app/boids/boidsFunctions.ts
+++ src/app/boids/boidsFunctions.ts
@@ -1,1 +1,14 @@
+/*
+So this is an exciting project for me, I have always marveled at these shapes and to direct my computer towards creating them is an exciting step.
+
+these classes will define the boid object,
+
+the accelerations and vectors for the objects,
+
+and the interactions between them,
+ie all the operations required in the relationing of the boids
+
+
+*/
+
 export class Boid {}
