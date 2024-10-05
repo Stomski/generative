75972f91f365d739d601058412ad7a1d5ec10dd3
Robert Stomski <stomski.robert@gmail.com>
2024-10-05 16:16:12 -0600

    boids start

---
+++ src/app/boids/boids.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boids',
+  standalone: true,
+  imports: [],
+  templateUrl: './boids.component.html',
+  styleUrl: './boids.component.css'
+})
+export class BoidsComponent {
+
+}
