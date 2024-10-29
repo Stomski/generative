c6b913b733b98f02a90792ee18730ceec7488188
Robert Stomski <stomski.robert@gmail.com>
2024-10-29 07:56:33 -0600

    boids

--- src/app/boids/boidsFunctions.ts
+++ src/app/boids/boidsFunctions.ts
@@ -11,12 +11,41 @@ ie all the operations required in the relationing of the boids
 
 */
 
-export class Boid {
-  //constructor
-  constructor() {
-    this.position = 2;
+export class Vec2 {
+  x: number;
+  y: number;
+
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
   }
 
-  //boids haveeeee velocity, acceleration, position
-  //
+  // Method to add another Vec2 to this vector
+  add(vector: Vec2): Vec2 {
+    return new Vec2(this.x + vector.x, this.y + vector.y);
+  }
+}
+
+export class Boid {
+  position: Vec2;
+  velocity: Vec2;
+  acceleration: Vec2;
+
+  constructor(
+    position = new Vec2(),
+    velocity = new Vec2(),
+    acceleration = new Vec2()
+  ) {
+    this.position = position;
+    this.velocity = velocity;
+    this.acceleration = acceleration;
+  }
+
+  // Example method to update boid's position based on velocity and acceleration
+  update(): void {
+    this.velocity = this.velocity.add(this.acceleration);
+    this.position = this.position.add(this.velocity);
+    // Reset acceleration after each update
+    this.acceleration = new Vec2();
+  }
 }
